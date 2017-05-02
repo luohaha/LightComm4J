c137303b2a600d160176dd6ecbfe9a32c15dcce0
Yixin Luo <18810541851@163.com>
2017-05-02 21:21:14 +0800

    update test

--- LightComm4J/src/test/java/com/github/luohaha/server/test/TestServer.java
+++ LightComm4J/src/test/java/com/github/luohaha/server/test/TestServer.java
@@ -1,6 +1,7 @@
 package com.github.luohaha.server.test;
 
 import java.io.IOException;
+import java.net.SocketAddress;
 
 import com.github.luohaha.param.ServerParam;
 import com.github.luohaha.server.LightCommServer;
@@ -11,22 +12,31 @@ public class TestServer {
 		try {
 			ServerParam param = new ServerParam("localhost", 8888);
 			param.setBacklog(128);
-			param.setOnAccept((conn) -> {
+			param.setOnAccept(conn -> {
 				System.out.println("accept!");
+				try {
+					conn.setKeepAlive(false);
+					SocketAddress address = conn.getRemoteAddress();
+					System.out.println(address.toString());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			});
 			param.setOnRead((conn, data) -> {
 				System.out.println("read!");
 			});
-			param.setOnWrite((conn) -> {
+			param.setOnWrite(conn -> {
 				try {
 					conn.write(String.valueOf(System.currentTimeMillis()).getBytes());
+					System.out.println(conn.getKeepAlive());
 					conn.close();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			});
-			param.setOnClose((conn) -> {
+			param.setOnClose(conn -> {
 				System.out.println("close!");
 				try {
 					conn.doClose();
